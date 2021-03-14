478e099a593dcc3d94372253b7736540c11006fe
keeplooking6 <1422910966@qq.com>
2021-03-14 17:02:45 +0800

    feat: add some dependencies and springboot framework

---
+++ src/main/java/edu/zc/oj/JudgeBEApplication.java
@@ -0,0 +1,16 @@
+package edu.zc.oj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author keep-looking
+ * @date 2021/3/14 - 16:40
+ */
+
+@SpringBootApplication
+public class JudgeBEApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JudgeBEApplication.class, args);
+    }
+}
